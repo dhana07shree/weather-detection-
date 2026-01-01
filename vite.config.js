56d8bbb0ef9073b3809088e24323ba51f59f62c7
Dhanashree Chandekar <dchandekar2006@gmail.com>
2026-01-01 21:13:23 +0530

    Deploy React app via docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mini-project-react/'
+  base: '/weather-detection-/'
 })
